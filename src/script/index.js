b3e172315c044c8d37203a0bd83941a722e100ee
manasgulatiJoshTech <manas.gulati@joshtechnologygroup.com>
2026-08-03 19:08:05 +0530

    [MG_A1_02]: Add hero layout (#3)
    * [MG_A1_02]: Add hero layout
    
    * [MG_A1_02]: Some code fixes
    
    * [MG_A1_03]: Add Service Section (#4)
    
    * [MG_A1_03]: Add service layout
    
    * [MG_A1_03]: add Service section
    
    * [MG_A1_03]: Add Service Section

--- src/script/index.js
+++ src/script/index.js
@@ -14,8 +14,8 @@ const navBox = document.querySelector('.container__nav-box');
 const navLink = document.querySelector('.container__nav-link');
 const buttonLinksPrimary = data.header.buttons[0];
 const buttonLinksSecondary = data.header.buttons[1];
-const btnPrimary = document.querySelectorAll('.button--primary');
-const btnSecondary = document.querySelectorAll('.button--secondary');
+const btnPrimary = document.querySelectorAll('.container__btn-primary');
+const btnSecondary = document.querySelectorAll('.container__btn-secondary');
 
 /** fetch required data and append it to necessary container
  *
@@ -63,3 +63,58 @@ menu.addEventListener('click', () => {
     menu.setAttribute('aria-expanded', ans);
     menu.setAttribute('aria-label', ariaLabel);
 });
+
+//Header section ends
+
+//Hero Section starts
+
+//accessing required data
+const heroSubHeading = data.hero.subHeading;
+const heroHeading = data.hero.heading[0];
+const heroSubtitle = data.hero.subtitle;
+
+//function to add text to required container
+function addText(element, container, text) {
+    let para = document.createElement(element);
+    document.querySelector(container).prepend(para);
+    para.innerHTML = text;
+}
+//Adding sub Heading
+addText('p', '.hero__content-sub-heading', heroSubHeading);
+
+//Adding Heading
+let details = `${heroHeading.slice(0, data.hero.heading[1])} <span class="highlight">${heroHeading.slice(data.hero.heading[1], data.hero.heading[2])}</span> ${heroHeading.slice(data.hero.heading[2])}`;
+addText('p', '.hero__content-heading', details);
+
+//adding subtitle
+addText('p', '.hero__content-subtitle', heroSubtitle);
+
+//Hero Section ends
+
+//Service Section starts
+
+//accessing required data
+const serviceSubHeading = data.service.subHeading;
+const serviceHeading = data.service.heading;
+const serviceSubtitle = data.service.subtitle;
+
+//Adding sub Heading
+addText('p', '.service__content-sub-heading', serviceSubHeading);
+
+//Adding Heading
+addText('p', '.service__content-heading', serviceHeading);
+
+//adding subtitle
+addText('p', '.service__content-subtitle', serviceSubtitle);
+
+//setting card data
+const cardData = data.card;
+Object.values(cardData).forEach((card) => {
+    let cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+    cardContainer.classList.add('service__card-block');
+    cardContainer.innerHTML = `<h4> ${card.number} </h4><p class="service__card-desc">${card.heading}</p>`;
+    document.querySelector('.service__card-layout').appendChild(cardContainer);
+});
+
+//Service Section ends
